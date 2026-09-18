81da2a151693570274b0d2f8555fa63b2e722e60
Blexyel <passihd@phoenixts.eu>
2026-09-18 17:17:43 +0200

    feat: 26.3

--- src/main/java/wtf/blexyel/hudsy/Keybindings.java
+++ src/main/java/wtf/blexyel/hudsy/Keybindings.java
@@ -1,6 +1,7 @@
 package wtf.blexyel.hudsy;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.platform.InputConstants.Type;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.KeyMapping.Category;
@@ -10,10 +11,10 @@ public class Keybindings {
   public static Category CATEGORY = KeyMapping.Category.register(Identifier.parse("hudsy.main"));
 
   public static final KeyMapping ENABLED_KEY =
-      new KeyMapping("key.hudsy.enable", InputConstants.Type.KEYSYM, -1, CATEGORY);
+      new KeyMapping("key.hudsy.enable", Type.KEYBOARD, -1, CATEGORY);
 
   public static final KeyMapping CONFIG_KEY =
-      new KeyMapping("key.hudsy.config", InputConstants.Type.KEYSYM, -1, CATEGORY);
+      new KeyMapping("key.hudsy.config", Type.KEYBOARD, -1, CATEGORY);
 
   public static void init() {
     KeyMappingHelper.registerKeyMapping(ENABLED_KEY);
